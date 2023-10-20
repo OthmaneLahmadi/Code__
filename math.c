7bbd24f9534981c957c27be06cbdae45bccf038c
lahmadi_othmane <otmanwacreal20765@gmail.com>
2023-10-21 00:48:19 +0100

    version 1.2

--- math.c
+++ math.c
@@ -1,8 +1,13 @@
+//sommation de a & b
 int add(int a,int b){
 return a+b;
 }
 
-
+//soustraction : a-b 
 int sub(int a,int b){
 return a-b;
+}
+// multiplication a x b
+int mul(int a,int b){
+return a*b;
 }
--- math.h
+++ math.h
@@ -2,4 +2,5 @@
 #define __math
 int add(int a,int b);
 int sub(int a,int b);
+int mul(int a,int b);
 #endif
