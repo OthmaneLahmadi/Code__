0007589201739f0d89851aa6ac228fc3377cf99f
lahmadi_othmane <otmanwacreal20765@gmail.com>
2023-10-21 12:39:41 +0100

    version 1.6

--- string.c
+++ string.c
@@ -7,3 +7,15 @@ else break;
 }
 return counter;
 }
+
+char str_comp(char *a,char* b){
+unsigned char x;
+if((x=len(a))!=len(b))return 0;
+char k=0;
+while(1){
+if(a[k]!=b[k])return 0;
+else if(a[k]!='\0')k++;
+else
+	return k==x ? 1:0;
+}
+}
--- string.h
+++ string.h
@@ -1,4 +1,5 @@
 #ifndef __string
 #define __string
 int len(char *s);
+char str_comp(char *a,char* b);
 #endif
